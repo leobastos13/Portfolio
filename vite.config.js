19669f3eeccf22103b5faefeb9c2f83dbdac12c7
leobastos13 <126288005+leobastos13@users.noreply.github.com>
2024-06-03 18:22:07 +0100

    feat: vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import path from 'path';
 
 export default defineConfig({
-    base: "./",
+    base: "/Portfolio",
     build: {
         minify: "terser",
     },
